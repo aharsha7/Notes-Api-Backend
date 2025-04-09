5e8136758f049c705a1974cede3ce8b7dc5204ba
aharsha7 <aharsha7na@gmail.com>
2025-04-09 18:50:39 +0530

    mongodb url

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/api/notes', noteRoutes);
 
